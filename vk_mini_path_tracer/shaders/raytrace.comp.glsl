805bed20b751e7e53e3570bd6692da3a96e722e9
宇半仙 <jingyu.xiong@foxmail.com>
2023-10-08 09:59:49 +0800

    add antialiasing and diffuse

--- vk_mini_path_tracer/shaders/raytrace.comp.glsl
+++ vk_mini_path_tracer/shaders/raytrace.comp.glsl
@@ -30,6 +30,20 @@ float stepAndOutputRNGFloat(inout uint rngState)
   return float(word) / 4294967295.0f;
 }
 
+const float k_pi = 3.14159265;
+
+// Uses the Box-Muller transform to return a normally distributed (centered
+// at 0, standard deviation 1) 2D point.
+vec2 randomGaussian(inout uint rngState)
+{
+  // Almost uniform in (0, 1] - make sure the value is never 0:
+  const float u1    = max(1e-38, stepAndOutputRNGFloat(rngState));
+  const float u2    = stepAndOutputRNGFloat(rngState);  // In [0, 1]
+  const float r     = sqrt(-2.0 * log(u1));
+  const float theta = 2 * k_pi * u2;  // Random in [0, 2pi]
+  return r * vec2(cos(theta), sin(theta));
+}
+
 vec3 skyColor(vec3 direction)
 {
   if(direction.y > 0.0f)
@@ -92,50 +106,73 @@ void main()
     return;
   }
 
+  // State of the random number generator.
+  uint rngState = resolution.x * pixel.y + pixel.x;  // Initial seed
+
   const vec3 cameraPosition = vec3(-0.001, 1.0, 6.0);
   const float fovVerticalSlope = 1.0 / 5.0;
   const float aspectRatio = float(resolution.x) / float(resolution.y);
 
-  vec3 rayOrigin = cameraPosition;
-
-  const vec2 screenUV = (2.0 * vec2(pixel + 0.5) / vec2(resolution) - 1.0);
-  vec3 rayDirection = vec3(fovVerticalSlope * screenUV * vec2(aspectRatio, 1), -1.0);
-
-  vec3 accumulatedRayColor = vec3(1.0);
-  vec3 pixelColor          = vec3(0.0);
-
-  // trace the ray
-  for (int tracedSegments = 0; tracedSegments < 32; tracedSegments++)
+  // The sum of the colors of all of the samples.
+  vec3 summedPixelColor = vec3(0.0);
+  // Limit the kernel to trace at most 64 samples.
+  const int NUM_SAMPLES = 64;
+  for(int sampleIdx = 0; sampleIdx < NUM_SAMPLES; sampleIdx++)
   {
-    rayQueryEXT rayQuery;
-    rayQueryInitializeEXT(rayQuery, tlas, gl_RayFlagsNoneEXT, 0xFF, rayOrigin, 0.0, rayDirection, 10000.0);
 
-    // get closet hit
-    while (rayQueryProceedEXT(rayQuery))
+    vec3 rayOrigin = cameraPosition;
+
+    // Use a Gaussian with standard deviation 0.375 centered at the center of the pixel:
+    const vec2 randomPixelCenter = vec2(pixel) + 0.5 + 0.375 * randomGaussian(rngState);
+    const vec2 screenUV = 2.0 * randomPixelCenter / vec2(resolution) - 1.0;
+    vec3 rayDirection = vec3(fovVerticalSlope * screenUV * vec2(aspectRatio, 1), -1.0);
+
+    vec3 accumulatedRayColor = vec3(1.0);
+    vec3 pixelColor          = vec3(0.0);
+
+    // trace the ray
+    for (int tracedSegments = 0; tracedSegments < 32; tracedSegments++)
     {
+      rayQueryEXT rayQuery;
+      rayQueryInitializeEXT(rayQuery, tlas, gl_RayFlagsNoneEXT, 0xFF, rayOrigin, 0.0, rayDirection, 10000.0);
+
+      // get closet hit
+      while (rayQueryProceedEXT(rayQuery))
+      {
+      }
+
+      // check if the ray hit something
+      if(rayQueryGetIntersectionTypeEXT(rayQuery, true) == gl_RayQueryCommittedIntersectionTriangleEXT)
+      {
+        HitInfo hitInfo = getObjectHitInfo(rayQuery);
+
+        hitInfo.worldNormal = faceforward(hitInfo.worldNormal, rayDirection, hitInfo.worldNormal);
+
+        accumulatedRayColor *= hitInfo.color;
+
+        rayOrigin = hitInfo.worldPosition + 0.0001 * hitInfo.worldNormal;
+
+        const float theta = 2.0 * k_pi * stepAndOutputRNGFloat(rngState);  // Random in [0, 2pi]
+        const float u     = 2.0 * stepAndOutputRNGFloat(rngState) - 1.0;  // Random in [-1, 1]
+        const float r     = sqrt(1.0 - u * u);
+        rayDirection      = hitInfo.worldNormal + vec3(r * cos(theta), r * sin(theta), u);
+        // Then normalize the ray direction:
+        rayDirection = normalize(rayDirection);
+      }
+      else
+      {
+        // Ray hit the sky
+        accumulatedRayColor *= skyColor(rayDirection);
+
+        // Sum this with the pixel's other samples.
+        summedPixelColor += accumulatedRayColor;
+
+        break;
+      }
+
     }
-
-    // check if the ray hit something
-    if(rayQueryGetIntersectionTypeEXT(rayQuery, true) == gl_RayQueryCommittedIntersectionTriangleEXT)
-    {
-      HitInfo hitInfo = getObjectHitInfo(rayQuery);
-
-      hitInfo.worldNormal = faceforward(hitInfo.worldNormal, rayDirection, hitInfo.worldNormal);
-
-      accumulatedRayColor *= hitInfo.color;
-
-      rayOrigin = hitInfo.worldPosition + 0.0001 * hitInfo.worldNormal;
-      rayDirection = reflect(rayDirection, hitInfo.worldNormal);
-    }
-    else
-    {
-      // Ray hit the sky
-      pixelColor = accumulatedRayColor * skyColor(rayDirection);
-      break;
-    }
-
   }
 
   uint index = resolution.x * pixel.y + pixel.x;
-  imageData[index] = pixelColor;
+  imageData[index] = summedPixelColor / float(NUM_SAMPLES);;
 }
